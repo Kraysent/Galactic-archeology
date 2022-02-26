32ba9c6ec8833aaadb9c05d3905d457a53c6ae13
Artyom Zaporozhets <kraysent@gmail.com>
2022-02-26 18:48:11 +0300

    #60: scatter and time evolution tasks moved to separate files

--- omtool/analysis/tasks/abstract_task.py
+++ omtool/analysis/tasks/abstract_task.py
@@ -2,12 +2,9 @@ from abc import ABC, abstractmethod
 from typing import Tuple
 
 import numpy as np
-from amuse.lab import units, Particles
-from amuse.units.quantities import VectorQuantity, ScalarQuantity
+from amuse.lab import Particles, units
 from omtool.datamodel import Snapshot
-from physical_evaluator import Parser
 
-from omtool.datamodel import profiler
 
 class AbstractTask(ABC):
     @abstractmethod
@@ -45,48 +42,3 @@ class AbstractTimeTask(AbstractTask):
     def _as_tuple(self) -> Tuple[np.ndarray, np.ndarray]:
         return (np.array(self.times), np.array(self.values))
     
-class ScatterTask(AbstractTask):
-    def __init__(self, x_expr: str, y_expr: str, x_unit: ScalarQuantity, y_unit: ScalarQuantity):
-        parser = Parser()
-
-        self.x_unit = x_unit
-        self.y_unit = y_unit
-        self.x_expr = parser.parse(x_expr)
-        self.y_expr = parser.parse(y_expr)
-
-    @profiler('Scatter task')
-    def run(self, snapshot: Snapshot) -> Tuple[np.ndarray, np.ndarray]:
-        particles = self.filter_barion_particles(snapshot)
-        params = self._get_sliced_parameters(particles)
-        
-        x_value = self.x_expr.evaluate(params)
-        y_value = self.y_expr.evaluate(params)
-
-        return (x_value / self.x_unit, y_value / self.y_unit)
-
-class TimeEvolutionTask(AbstractTask):
-    functions = {
-        'sum': np.sum,
-        'mean': np.mean,
-        'none': lambda x: x
-    }
-
-    def __init__(self, expr: str, time_unit: ScalarQuantity, value_unit: ScalarQuantity,  function: str = 'none'):
-        parser = Parser()
-
-        self.expr = parser.parse(expr)
-        self.function = self.functions[function]
-        self.time_unit = time_unit
-        self.value_unit = value_unit
-        self.times = VectorQuantity([], time_unit.unit)
-        self.values = VectorQuantity([], value_unit.unit)
-
-    @profiler('Time evolution task')
-    def run(self, snapshot: Snapshot) -> Tuple[np.ndarray, np.ndarray]:
-        value = self.expr.evaluate(self._get_sliced_parameters(snapshot.particles))
-        value = self.function(value)
-
-        self.times.append(snapshot.timestamp)
-        self.values.append(value)
-
-        return (self.times / self.time_unit, self.values / self.value_unit)
---
+++ omtool/analysis/tasks/time_evolution_task.py
@@ -0,0 +1,35 @@
+from typing import Tuple
+
+import numpy as np
+from amuse.lab import ScalarQuantity, VectorQuantity
+from omtool.analysis.tasks import AbstractTask
+from omtool.datamodel import Snapshot, profiler
+from physical_evaluator import Parser
+
+
+class TimeEvolutionTask(AbstractTask):
+    functions = {
+        'sum': np.sum,
+        'mean': np.mean,
+        'none': lambda x: x
+    }
+
+    def __init__(self, expr: str, time_unit: ScalarQuantity, value_unit: ScalarQuantity,  function: str = 'none'):
+        parser = Parser()
+
+        self.expr = parser.parse(expr)
+        self.function = self.functions[function]
+        self.time_unit = time_unit
+        self.value_unit = value_unit
+        self.times = VectorQuantity([], time_unit.unit)
+        self.values = VectorQuantity([], value_unit.unit)
+
+    @profiler('Time evolution task')
+    def run(self, snapshot: Snapshot) -> Tuple[np.ndarray, np.ndarray]:
+        value = self.expr.evaluate(self._get_sliced_parameters(snapshot.particles))
+        value = self.function(value)
+
+        self.times.append(snapshot.timestamp)
+        self.values.append(value)
+
+        return (self.times / self.time_unit, self.values / self.value_unit)
