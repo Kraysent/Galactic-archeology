#14: fix potential center computation

omtool/analysis/tasks/potential_task.py
@@ -3,18 +3,17 @@ from typing import Tuple
 import numpy as np
 from amuse.lab import units
 from omtool.analysis.tasks import AbstractTask
-from omtool.analysis.utils import math, pyfalcon_analizer
-from omtool.datamodel import Snapshot
-from omtool.datamodel import profiler
+from omtool.analysis.utils import math, particle_centers, pyfalcon_analizer
+from omtool.datamodel import Snapshot, profiler
 
 
 class PotentialTask(AbstractTask):
     @profiler('Potential task')
     def run(self, snapshot: Snapshot) -> Tuple[np.ndarray, np.ndarray]:
-        cm = snapshot.particles.center_of_mass()
+        center = particle_centers.center_of_mass(snapshot.particles)
         particles = snapshot.particles
 
-        r = math.get_lengths(particles.position - cm)
+        r = math.get_lengths(particles.position - center)
         potentials = pyfalcon_analizer.get_potentials(snapshot.particles, 0.2 | units.kpc)
         (r, potentials) = math.sort_with(r, potentials)
 

omtool/analysis/utils/particle_centers.py
@@ -1,6 +1,7 @@
+import numpy as np
 from amuse.lab import Particles, VectorQuantity, units
 from omtool.analysis.utils import pyfalcon_analizer
-import numpy as np
+
 
 def center_of_mass(particles: Particles) -> VectorQuantity:
     return particles.center_of_mass()
@@ -19,6 +20,4 @@ def potential_center(particles: Particles) -> VectorQuantity:
     masses = particles.mass[perm]
     masses = masses[:int(len(masses) * top_percent)]
 
-    center = np.sum(positions * masses[:, np.newaxis]) / np.sum(masses)
-
-    return center
+    return np.sum(positions * masses[:, np.newaxis], axis = 0) / np.sum(masses)