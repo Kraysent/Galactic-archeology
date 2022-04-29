960523cb6734257d52cf8206fff44610cb92334e
Artyom Zaporozhets <kraysent@gmail.com>
2022-04-29 23:57:24 +0300

    #78: remove legacy logger module

--- logger/__init__.py
+++
@@ -1,6 +0,0 @@
-'''
-Logger service that provides functionality to print logs
-into different destinations.
-'''
-from logger.config import Config
-from logger.main import debug, error, info, initialize, warning
