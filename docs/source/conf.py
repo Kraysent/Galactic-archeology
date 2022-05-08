50e820c76a8a41c5b2656f2f4f91d5406f9bbe8b
Artyom Zaporozhets <kraysent@gmail.com>
2022-05-08 23:04:21 +0300

    #36: theme update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,23 +1,26 @@
+import sphinx_pdj_theme
+
 # -- Project information -----------------------------------------------------
 
-project = 'OMTool'
-copyright = '2022, Kraysent'
-author = 'Kraysent'
+project = "OMTool"
+copyright = "2022, Kraysent"
+author = "Kraysent"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
-
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 
-extensions = [
-]
+extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_pdj_theme"
+htm_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+html_theme_options = {"style": "darker"}
+
+html_static_path = ["_static"]
