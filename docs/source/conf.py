79192716feed156cd86314e54c7de0276132caf8
Artyom Zaporozhets <kraysent@gmail.com>
2022-05-08 21:53:51 +0300

    #36: minimal rst documentation added

---
+++ docs/source/conf.py
@@ -0,0 +1,23 @@
+# -- Project information -----------------------------------------------------
+
+project = 'OMTool'
+copyright = '2022, Kraysent'
+author = 'Kraysent'
+
+# The full version, including alpha/beta/rc tags
+release = '0.1.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+]
+
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
