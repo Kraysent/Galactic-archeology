53a29a854fd8acd49d23bd7488f7382d8e0f47e8
Artyom Zaporozhets <kraysent@gmail.com>
2022-05-10 12:38:17 +0300

    #87: tasks are now optional in analysis

--- omtool/core/analysis/config.py
+++ omtool/core/analysis/config.py
@@ -1,53 +1,67 @@
+"""
+Configuration classes for analysis tool.
+"""
 from typing import Any, List
 
-from omtool import io_service
-from omtool import visualizer
 import yaml
+from omtool import io_service, visualizer
 from omtool.core.analysis.tasks import get_task
 from omtool.core.datamodel import required_get, yaml_loader
 
 
 class TaskConfig:
+    """
+    Configuration for each particular task.
+    """
+
     slice: slice
-    abstract_task: Any # AbstractTask actually
+    abstract_task: Any  # AbstractTask actually
     handlers: dict
-    
+
     @staticmethod
-    def from_dict(input: dict) -> 'TaskConfig':
+    def from_dict(data: dict) -> "TaskConfig":
+        """
+        Construct this object from dictionary.
+        """
         res = TaskConfig()
-        res.slice = slice(*input.get('slice', [0, None, 1]))
-        res.handlers = input.get('handlers', { })
-        res.abstract_task = get_task(
-            required_get(input, 'name'), 
-            input.get('args', { })
-        )
+        res.slice = slice(*data.get("slice", [0, None, 1]))
+        res.handlers = data.get("handlers", {})
+        res.abstract_task = get_task(required_get(data, "name"), data.get("args", {}))
 
         return res
 
+
 class AnalysisConfig:
+    """
+    General configuration for analysis tool.
+    """
+
     input_file: io_service.Config
     visualizer: visualizer.Config
     tasks: List[TaskConfig]
     plot_interval: slice
 
     @staticmethod
-    def from_yaml(filename: str) -> 'AnalysisConfig':
+    def from_yaml(filename: str) -> "AnalysisConfig":
+        """
+        Construct this object from YAML file.
+        """
         data = {}
 
-        with open(filename, 'r') as stream:
-            data = yaml.load(stream, Loader = yaml_loader())
-        
+        with open(filename, "r", encoding="utf-8") as stream:
+            data = yaml.load(stream, Loader=yaml_loader())
+
         return AnalysisConfig.from_dict(data)
 
     @staticmethod
-    def from_dict(input: dict) -> 'AnalysisConfig':
+    def from_dict(data: dict) -> "AnalysisConfig":
+        """
+        Construct this object from dictionary.
+        """
         res = AnalysisConfig()
-        res.tasks = [
-            TaskConfig.from_dict(task) for task in required_get(input, 'tasks')
-        ]
-        res.plot_interval = slice(*input.get('plot_interval', [0, None, 1]))
-        res.visualizer = visualizer.Config.from_dict(required_get(input, 'visualizer'))
-        res.input_file = io_service.Config.from_dict(required_get(input, 'input_file'))
+        res.tasks = [TaskConfig.from_dict(task) for task in data.get("tasks", [])]
+        res.plot_interval = slice(*data.get("plot_interval", [0, None, 1]))
+        res.visualizer = visualizer.Config.from_dict(required_get(data, "visualizer"))
+        res.input_file = io_service.Config.from_dict(required_get(data, "input_file"))
 
         return res
-
