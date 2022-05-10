6a79320ce0939793d9871f1ff2927de5d17904d5
Artyom Zaporozhets <kraysent@gmail.com>
2022-05-10 15:44:59 +0300

    #97: user can now perform analysis task during integration

--- integration.py
+++ integration.py
@@ -1,30 +1,75 @@
-'''
+"""
 Integration module for OMTool. Used to integrate existing model
 from the file and write it to another file.
-'''
+"""
 import os
 from pathlib import Path
+from typing import Tuple
 
 from amuse.lab import units
+import numpy as np
 
-from omtool import io_service
+from omtool import io_service, visualizer
 from omtool import json_logger as logger
+from omtool.core.analysis.visual import VisualTask
 from omtool.core.datamodel import Snapshot, profiler
 from omtool.core.integration.integrators import get_integrator
 from omtool.core.integration.config import IntegrationConfig
 
 
+def logger_handler(data: Tuple[np.ndarray, np.ndarray], parameters: dict = None):
+    """
+    Handler that logs ndarrays to the INFO level.
+    """
+    if parameters is None:
+        parameters = {}
+
+    if parameters["print_last"]:
+        logger.info(
+            message_type=parameters["id"],
+            payload={"x": data[0].tolist()[-1], "y": data[1].tolist()[-1]},
+        )
+    else:
+        logger.info(
+            message_type=parameters["id"],
+            payload={"x": data[0].tolist(), "y": data[1].tolist()},
+        )
+
+
 def integrate(config: IntegrationConfig):
-    '''
+    """
     Integration mode for the OMTool. Used to integrate existing model
     from the file and write it to another file.
-    '''
+    """
     input_service = io_service.InputService(config.input_file)
     integrator = get_integrator(
         config.integrator.name,
         Snapshot(*next(input_service.get_snapshot_generator())),
-        config.integrator.args
+        config.integrator.args,
     )
+
+    handlers = {}
+    handlers["logging"] = logger_handler
+
+    visualizer_service = None
+
+    if not config.visualizer is None:
+        visualizer_service = visualizer.VisualizerService(config.visualizer)
+        handlers["visualizer"] = visualizer_service.plot
+
+    tasks = [
+        VisualTask(
+            task.abstract_task,
+            task.slice,
+            [
+                lambda data, key=key, handler_params=value: handlers[key](
+                    data, handler_params
+                )
+                for key, value in task.handlers.items()
+            ],
+        )
+        for task in config.tasks
+    ]
 
     if config.overwrite:
         if Path(config.output_file).is_file():
@@ -35,41 +80,54 @@ def integrate(config: IntegrationConfig):
                 f'Output file ({config.output_file}) exists and "overwrite" option in integration config file is false (default)'
             )
 
-    @profiler('Integration stage')
-    def loop_integration_stage():
+    @profiler("Integration stage")
+    def loop_integration_stage() -> Snapshot:
         integrator.leapfrog()
 
-    @profiler('Saving to file stage')
-    def loop_saving_stage(iteration: int):
+        return integrator.get_snapshot()
+
+    @profiler("Analysis stage")
+    def loop_analysis_stage(iteration: int, snapshot: Snapshot):
+        for vtask in tasks:
+            vtask.run(snapshot)
+
+        if not visualizer_service is None:
+            visualizer_service.save(
+                {"i": iteration, "time": snapshot.timestamp.value_in(units.Myr)}
+            )
+
+    @profiler("Saving to file stage")
+    def loop_saving_stage(iteration: int, snapshot: Snapshot):
         if iteration % config.snapshot_interval == 0:
-            snapshot = integrator.get_snapshot()
             snapshot.to_fits(config.output_file, append=True)
 
         for log in config.logs:
             particle = integrator.get_particle(log.point_id)
-            logger.info(message_type=log.logger_id, payload={
-                'timestamp': integrator.timestamp.value_in(units.Myr),
-                'x': particle.x.value_in(units.kpc),
-                'y': particle.y.value_in(units.kpc),
-                'z': particle.z.value_in(units.kpc),
-                'vx': particle.vx.value_in(units.kms),
-                'vy': particle.vy.value_in(units.kms),
-                'vz': particle.vz.value_in(units.kms),
-                'm': particle.mass.value_in(units.MSun)
-            })
+            logger.info(
+                message_type=log.logger_id,
+                payload={
+                    "timestamp": integrator.timestamp.value_in(units.Myr),
+                    "x": particle.x.value_in(units.kpc),
+                    "y": particle.y.value_in(units.kpc),
+                    "z": particle.z.value_in(units.kpc),
+                    "vx": particle.vx.value_in(units.kms),
+                    "vy": particle.vy.value_in(units.kms),
+                    "vz": particle.vz.value_in(units.kms),
+                    "m": particle.mass.value_in(units.MSun),
+                },
+            )
 
         logger.info(
-            message_type='integration_timing',
-            payload={
-                'timestamp': integrator.timestamp.value_in(units.Myr)
-            }
+            message_type="integration_timing",
+            payload={"timestamp": integrator.timestamp.value_in(units.Myr)},
         )
 
-    logger.info('Integration started')
+    logger.info("Integration started")
     i = 0
 
     while integrator.timestamp < config.model_time:
-        loop_integration_stage()
-        loop_saving_stage(i)
+        snapshot = loop_integration_stage()
+        loop_analysis_stage(i, snapshot)
+        loop_saving_stage(i, snapshot)
 
         i += 1
--- omtool/core/integration/config.py
+++ omtool/core/integration/config.py
@@ -1,55 +1,60 @@
-'''
+"""
 Confuration objects' description for integration module.
-'''
+"""
 from typing import List
 import yaml
 from amuse.lab import ScalarQuantity
 from omtool import io_service
+from omtool.core.analysis.config import TaskConfig
 from omtool.core.datamodel import required_get, yaml_loader
+from omtool import visualizer
 
 
 class LogParamsConfig:
-    '''
+    """
     Configuration of logging parameters for particles.
-    '''
+    """
+
     point_id: int
     logger_id: str
 
     @staticmethod
-    def from_dict(data: dict) -> 'LogParamsConfig':
-        '''
+    def from_dict(data: dict) -> "LogParamsConfig":
+        """
         Loads this type from dictionary.
-        '''
+        """
         res = LogParamsConfig()
-        res.point_id = required_get(data, 'point_id')
-        res.logger_id = required_get(data, 'logger_id')
+        res.point_id = required_get(data, "point_id")
+        res.logger_id = required_get(data, "logger_id")
 
         return res
 
 
 class IntegratorConfig:
-    '''
+    """
     Configuration for the particular integrator.
-    '''
+    """
+
     name: str
     args: dict
 
     @staticmethod
-    def from_dict(data: dict) -> 'IntegratorConfig':
-        '''
+    def from_dict(data: dict) -> "IntegratorConfig":
+        """
         Loads this type from dictionary.
-        '''
+        """
         res = IntegratorConfig()
-        res.name = required_get(data, 'name')
-        res.args = data.get('args', {})
+        res.name = required_get(data, "name")
+        res.args = data.get("args", {})
 
         return res
 
 
 class IntegrationConfig:
-    '''
+    """
     General configuration for integration.
-    '''
+    """
+
     input_file: io_service.Config
     output_file: str
     overwrite: bool
@@ -57,34 +62,38 @@ class IntegrationConfig:
     snapshot_interval: int
     integrator: IntegratorConfig
     logs: List[LogParamsConfig]
+    visualizer: visualizer.Config
+    tasks: List[TaskConfig]
 
     @staticmethod
-    def from_yaml(filename: str) -> 'IntegrationConfig':
-        '''
+    def from_yaml(filename: str) -> "IntegrationConfig":
+        """
         Loads this type from the actual YAML file.
-        '''
+        """
         data = {}
 
-        with open(filename, 'r', encoding='utf-8') as stream:
+        with open(filename, "r", encoding="utf-8") as stream:
             data = yaml.load(stream, Loader=yaml_loader())
 
         return IntegrationConfig.from_dict(data)
 
     @staticmethod
-    def from_dict(data: dict) -> 'IntegrationConfig':
-        '''
+    def from_dict(data: dict) -> "IntegrationConfig":
+        """
         Loads this type from dictionary.
-        '''
+        """
         res = IntegrationConfig()
-        res.input_file = io_service.Config.from_dict(
-            required_get(data, 'input_file'))
-        res.output_file = required_get(data, 'output_file')
-        res.overwrite = data.get('overwrite', False)
-        res.model_time = required_get(data, 'model_time')
-        res.snapshot_interval = data.get('snapshot_interval', 1)
-        res.integrator = IntegratorConfig.from_dict(required_get(data, 'integrator'))
-        res.logs = [
-            LogParamsConfig.from_dict(log) for log in data.get('logs', [])
-        ]
+        res.input_file = io_service.Config.from_dict(required_get(data, "input_file"))
+        res.output_file = required_get(data, "output_file")
+        res.overwrite = data.get("overwrite", False)
+        res.model_time = required_get(data, "model_time")
+        res.snapshot_interval = data.get("snapshot_interval", 1)
+        res.integrator = IntegratorConfig.from_dict(required_get(data, "integrator"))
+        res.logs = [LogParamsConfig.from_dict(log) for log in data.get("logs", [])]
+        res.tasks = [TaskConfig.from_dict(task) for task in data.get("tasks", [])]
+
+        res.visualizer = data.get("visualizer", None)
+        if not res.visualizer is None:
+            res.visualizer = visualizer.Config.from_dict(res.visualizer)
 
         return res
