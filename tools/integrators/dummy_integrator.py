3b0a41e309e87a5cbd50b8f7db1e20f9e33d69c4
Artyom Zaporozhets <kraysent@gmail.com>
2022-08-21 13:52:28 +0400

    #101: added dummy integrator

---
+++ tools/integrators/dummy_integrator.py
@@ -0,0 +1,26 @@
+from amuse.lab import ScalarQuantity
+
+from omtool.core.datamodel import AbstractIntegrator, Snapshot
+from omtool.core.integrators import register_integrator
+
+
+@register_integrator(name="dummy")
+class DummyIntegrator(AbstractIntegrator):
+    """
+    This is dummy integrator. It should only be used for development of core module
+    and is subject to change.
+
+    Propogates each particle of the snapshot along the velocity vector.
+
+    Args:
+    * `timestep` (`ScalarQuantity`): timestep of the iteration.
+    """
+
+    def __init__(self, timestep: ScalarQuantity) -> None:
+        self.timestep = timestep
+
+    def leapfrog(self, snapshot: Snapshot) -> Snapshot:
+        snapshot.particles.position += snapshot.particles.velocity * self.timestep
+        snapshot.timestamp += self.timestep
+
+        return snapshot
